0664918fd398cb1ffb5912fae54983d1b5344bef
Khushbu8848 <khushbu@8848digital.com>
2023-10-12 12:59:47 -0700

    Created Achivements Doc

---
+++ strategic_gears_cms_site_api/strategic_gears_cms_site_api/doctype/achievement_details/achievement_details.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, shyam and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class AchievementDetails(Document):
+	pass
