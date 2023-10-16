d494ad27640eddb64ee9fb43f65e9165abfe5ba0
aartimanjare <aartimanjare@gmail.com>
2023-10-16 15:59:25 +0530

    deleted master docs

--- strategic_gears_cms_site_api/strategic_gears_cms_site_api/doctype/client_master/client_master.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, shyam and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Client Master', {
-	// refresh: function(frm) {
-
-	// }
-});
--- strategic_gears_cms_site_api/strategic_gears_cms_site_api/doctype/service_details_master/service_details_master.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, shyam and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Service Details Master', {
-	// refresh: function(frm) {
-
-	// }
-});
