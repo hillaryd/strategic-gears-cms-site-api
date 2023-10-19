8279cd2d1d5fe216c5653117fce07c2599ae23d2
aartimanjare <aartimanjare@gmail.com>
2023-10-19 16:48:05 +0530

    api

--- strategic_gears_cms_site_api/api/V1.py
+++ strategic_gears_cms_site_api/api/V1.py
@@ -1,13 +1,17 @@
 import frappe
 from strategic_gears_cms_site_api.utils import success_response, error_response
 import strategic_gears_cms_site_api.api.v1.access_token as access_token
-
-
+import strategic_gears_cms_site_api.api.v1.about_strategic_gears as about_strategic_gears
+import strategic_gears_cms_site_api.api.v1.navbar as navbar
+import strategic_gears_cms_site_api.api.v1.team_member as team_member
 
 class V1():
     def __init__(self):
         self.methods = {
-            'access_token':['get_access_token']
+            'access_token':['get_access_token'],
+            'about_strategic_gears':['get_about_strategic_gears'],
+            'navbar':['get_navbar_data'],
+            'team_member':['get_team_member']
         }
 
     def class_map(self, kwargs):
